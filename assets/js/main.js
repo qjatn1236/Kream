58d6e9541c6333c851f8f67583359c9fb63e8895
qjatn1236@naver.com <qjatn1236@naver.com>
2022-07-11 13:48:45 +0900

    kream upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
-$(document).ready(function(){
-    $('.event_slide').slick({
-        dots: false,
-        arrows:false,
-        autoplay : true,
-    });
+$(document).ready(function () {
+  $('.event_slide').slick({
+    dots: false,
+    arrows: false,
+    autoplay: true,
+  });
+
+  //   $('.business_btn').click(function () {
+  //     $('.business_wrap').stop().slideToggle();
+  //   });
+
+  $('.business_btn').click(function () {
+    if ($('.business_wrap').css('display') == 'none') {
+      $('.business_wrap').show();
+    } else {
+      $('.business_wrap').hide();
+    }
+  });
 });
